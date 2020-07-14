fe680125ede5b5e35285521528793746cec6e2d0
Fernanda Peixoto <nanpeixoto@gmail.com>
2020-07-14 11:49:38 -0300

    TESTE

---
+++ src/main/java/br/com/ichef/service/UsuarioEmpresaoService.java
@@ -0,0 +1,9 @@
+package br.com.ichef.service;
+
+import br.com.ichef.dao.GenericDAO;
+import br.com.ichef.model.UsuarioEmpresa;
+
+public class UsuarioEmpresaoService extends GenericDAO<UsuarioEmpresa> {
+	private static final long serialVersionUID = 1L;
+
+}
